a9003529eb84d87cf7f97ddc8ea5a37f37d2139b
rvowles <rvowles@7ae50d05-d721-0410-be42-e1d25be70f31>
2006-12-03 05:38:42 +0000

    Fixed Ruby GEM to allow for building of our platform

--- gem/make_gem.rb
+++ gem/make_gem.rb
@@ -16,42 +16,30 @@ library       = nil
 platform      = nil
 fileset       = []
 major_version = 0
-minor_version = 4
-build_number  = 1
+minor_version = 5
+build_number  = 0
 version       = "#{major_version}.#{minor_version}.#{build_number}"
-if PLATFORM.include?('powerpc-darwin')
-   library  = 'fireruby.bundle'
-   platform = Gem::Platform::DARWIN
-elsif PLATFORM.include?('win32')
-   library  = 'fireruby.so'
+library  = 'ib_lib.so'
+if PLATFORM.include?('win32')
    platform = Gem::Platform::WIN32
 elsif PLATFORM.include?('linux')
-   library  = 'fireruby.so'
    platform = Gem::Platform::LINUX_586
 else
    raise "Unrecognised platform, gem creation cancelled."
 end
-gem = "fireruby-#{version}-#{platform}.gem"
+gem = "ibruby-#{version}-#{platform}.gem"
 
 # Check if we're not on Mac OS X.
-if platform == Gem::Platform::DARWIN
-   # Remove the .so library if it exists.
-   if File.exist?("#{libdir}#{File::SEPARATOR}fireruby.so")
-      puts "Removing the fireruby.so file from the lib directory."
-      rm("#{libdir}fireruby.so")
-   end
-else
    # Remove the .bundle library if it exists.
-   if File.exist?("#{libdir}#{File::SEPARATOR}fireruby.bundle")
-      puts "Removing the fireruby.bundle file from the lib directory."
-      rm("#{libdir}fireruby.bundle")
+   if File.exist?("#{libdir}#{File::SEPARATOR}ibruby.bundle")
+      puts "Removing the ibruby.bundle file from the lib directory."
+      rm("#{libdir}ibruby.bundle")
    end
-end
 
 # Remove any database file before creating the gem file.
 puts "Removing unneeded database files."
 Find.find(basedir) do |name|
-   if /.+\.fdb$/.match(name) != nil
+   if /.+\.ib$/.match(name) != nil
       rm(name)
    end
 end
@@ -64,11 +52,7 @@ Dir::chdir(basedir)
 # Check that the library exists.
 if File::exist?("#{srcdir}#{library}")
    puts "Copying '#{library}' into #{libdir}."
-   if platform == Gem::Platform::DARWIN
-      FileUtils::cp("#{srcdir}#{library}", "#{libdir}fr_lib.bundle")
-   else
-      FileUtils::cp("#{srcdir}#{library}", "#{libdir}fr_lib.so")
-   end
+   FileUtils::cp("#{srcdir}#{library}", "#{libdir}ib_lib.so")
 else
    raise "The #{library} library file does not exist in the #{srcdir} directory."
 end
@@ -77,21 +61,21 @@ end
 fileset = Dir.glob("{lib,test,doc,examples}/**/*")
 
 spec = Gem::Specification.new do |s|
-   s.name             = 'fireruby'
+   s.name             = 'ibruby'
    s.version          = version
-   s.author           = 'Peter Wood'
-   s.email            = 'paw220470@yahoo.ie'
-   s.homepage         = 'http://rubyforge.org/projects/fireruby/'
+   s.author           = 'Peter Wood, Richard Vowles'
+   s.email            = 'paw220470@yahoo.ie, rvowles@codegear.com'
+   s.homepage         = 'http://rubyforge.org/projects/ibruby/'
    s.platform         = platform
-   s.summary          = 'Ruby interface library for the Firebird database.'
-   s.description      = 'FireRuby is an extension to the Ruby programming '\
-                        'language that provides access to the Firebird open '\
-                        'source RDBMS. FireRuby is based in the Firebird C '\
-                        'API and has no additional dependencies. The FireRuby '\
+   s.summary          = 'Ruby interface library for the InterBase database.'
+   s.description      = 'IBRuby is an extension to the Ruby programming '\
+                        'language that provides access to the InterBase '\
+                        'RDBMS. IBRuby is based in the InterBase C '\
+                        'API and has no additional dependencies. The IBRuby '\
                         'library wraps the API calls in a Ruby OO interface.'
    s.files            = fileset
    s.require_path     = 'lib'
-   s.autorequire      = 'fireruby'
+   s.autorequire      = 'ibruby'
    s.test_files       = ["test#{File::SEPARATOR}UnitTest.rb"]
    s.has_rdoc         = true
    s.rdoc_options     = ["--main", "doc/README"]
